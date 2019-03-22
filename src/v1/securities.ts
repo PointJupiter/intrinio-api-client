20983f3e7213d027528d9800c6506ec1caa5a92e
Front End <frontend@Fronts-MacBook-Pro.local>
2019-03-22 11:11:05 +0100

    Add option to fetch historical data

--- src/v1/securities.ts
+++ src/v1/securities.ts
@@ -33,6 +33,24 @@ export class SecurityService {
     }
     return this.client.get('/data_point', { identifier, item })
   }
+
+  /**
+   * Returns the historical data for for a selected `identifier` (ticker symbol or index symbol)
+   * for a selected `tag`. Income statement, cash flow statement, and ratios are returned as trailing
+   * twelve months values by default, but can be changed with the type parameter. All other historical
+   * data points are returned as their value on a certain day based on filings reported as of that date.
+   *
+   * @param identifier An identifier, which can be a security ticker, SEC CIK ID, FRED Series ID, etc.
+   * @param item An item, which can be a data tag or a special code ([reference](https://data.intrinio.com/data-tags)).
+   * @param params Additional request params.
+   */
+  async historicalData (
+    identifier: string,
+    item: string,
+    params?: HistoricalDataForSecurityParams
+  ): Promise<HistoricalData> {
+    return this.client.get('/historical_data', { identifier, item, params })
+  }
 }
 
 interface DataPoint {
@@ -50,10 +68,93 @@ interface DataPoint {
   value: string | number
 }
 
-interface DataPointMulti {
+interface DataPointMulti extends BatchCallResponse {
   data: DataPoint[]
-  result_count: number
-  api_call_credits: number
 }
 
 type DataPointResponse = DataPoint | DataPointMulti
+
+interface HistoricalData extends BatchCallResponse {
+  data: {
+    /**
+     * The date associated with the value.
+     */
+    date: string;
+    /**
+     * The value.
+     */
+    value: number;
+  }[]
+  /**
+   * An identifier, which can be a security ticker, SEC CIK ID, FRED Series ID, etc.
+   */
+  identifier: string
+  /**
+   * An item, which can be a data tag or a special code ([reference](https://data.intrinio.com/data-tags)).
+   */
+  item: string
+  /**
+   * Number of results per page.
+   */
+  page_size: number
+  /**
+   * Current page.
+   */
+  current_page: number
+  /**
+   * Total number of pages.
+   */
+  total_pages: number
+}
+
+interface HistoricalDataForSecurityParams {
+  /**
+   * The earliest date for which to return data.
+   */
+  start_date?: string
+  /**
+   * The latest date for which to return data.
+   */
+  end_date?: string
+  /**
+   * The frequency of the historical prices & valuation data.
+   */
+  frequency?: 'daily' | 'weekly' | 'monthly' | 'quarterly' | 'yearly'
+  /**
+   * The type of historical data to fetch.
+   * For financial statements, the type reflects the type of financial statement.
+   * For Sector data, the type reflect the aggretation (mean, median, max, min) type of the data.
+   */
+  type?:
+    | 'FY'
+    | 'QTR'
+    | 'TTM'
+    | 'YTD'
+    | 'count'
+    | 'sum'
+    | 'max'
+    | '75thpctl'
+    | 'mean'
+    | 'median'
+    | '25thpctl'
+    | 'min'
+  /**
+   * An integer greater than or equal to 1 for specifying the page number for the return values.
+   */
+  page_number?: number
+  /**
+   * An integer greater than or equal to 1 for specifying the number of results on each page.
+   */
+  page_size?: number
+}
+
+interface BatchCallResponse {
+  /**
+   * Number of results returned in response.
+   */
+  result_count: number
+  /**
+   * Number of API call credits spent on a call.
+   */
+  api_call_credits: number
+}
--- test/v1/securities.spec.ts
+++ test/v1/securities.spec.ts
@@ -35,4 +35,51 @@ describe('securities', () => {
       scope.done()
     })
   })
+
+  describe('historicalData', () => {
+    test('returns the historical values for a specified identifier and item', async () => {
+      const params = {
+        identifier: 'AAPL',
+        item: 'marketcap'
+      }
+
+      const historicalData = [
+        {
+          date: '2019-03-20',
+          value: 898135096320
+        },
+        {
+          date: '2019-03-19',
+          value: 890354695560
+        },
+        {
+          date: '2019-03-18',
+          value: 897466841040
+        }
+      ]
+
+      const scope = nock(BASE_URL)
+        .get('/historical_data')
+        .query({ identifier: params.identifier, item: params.item })
+        .reply(200, {
+          data: historicalData,
+          identifier: 'AAPL',
+          item: 'marketcap',
+          result_count: 2899,
+          page_size: 1000,
+          current_page: 1,
+          total_pages: 3,
+          api_call_credits: 1
+        })
+
+      const response = await client.securities.historicalData(
+        params.identifier,
+        params.item
+      )
+
+      expect(response.data).toEqual(historicalData)
+
+      scope.done()
+    })
+  })
 })
