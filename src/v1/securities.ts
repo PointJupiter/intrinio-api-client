3b9180b004180974b4141654c32402a0f2e2affc
David Jelic <david@pointjupiter.com>
2019-03-25 13:52:13 +0100

    [v1/securities] Add doctype for `dataPoint` method

--- src/v1/securities.ts
+++ src/v1/securities.ts
@@ -3,9 +3,28 @@ import { IntrinioV1 } from '.'
 export class SecurityService {
   constructor (private readonly client: IntrinioV1.Intrinio) {}
 
-  async dataPoint (identifier: string, item: string | string[]): Promise<DataPoint>
-  async dataPoint (identifier: string[], item: string | string[]): Promise<DataPointMulti>
-  async dataPoint (identifier: string | string[], item: string | string[]): Promise<DataPointResponse> {
+  /**
+   * Returns the most recent data point for a selected identifier
+   * (ticker symbol, stock market index symbol, CIK ID, etc.) for a selected tag.
+   * Income statement, cash flow statement, and ratios are returned as trailing
+   * twelve months values. All other data points are returned as their most recent value,
+   * either as of the last release financial statement or the most recent reported value.
+   *
+   * @param identifier An identifier, which can be a security ticker, SEC CIK ID, FRED Series ID, etc.
+   * @param item An item, which can be a data tag or a special code ([reference](https://data.intrinio.com/data-tags)).
+   */
+  async dataPoint (
+    identifier: string,
+    item: string | string[]
+  ): Promise<DataPoint>
+  async dataPoint (
+    identifier: string[],
+    item: string | string[]
+  ): Promise<DataPointMulti>
+  async dataPoint (
+    identifier: string | string[],
+    item: string | string[]
+  ): Promise<DataPointResponse> {
     if (Array.isArray(identifier)) {
       identifier = identifier.join(',')
     }
